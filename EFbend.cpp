3a8b0fed3dd279ffbc8ad3ce4da37a600802e9c0
motykargar <38592901+motykargar@users.noreply.github.com>
2018-06-02 21:18:34 +0430

    Delete EFbend.cpp

--- EFbend.cpp
+++
@@ -1,9 +0,0 @@
-#include "EFbend.h"
-
-double EFbend(Cloth &cloth, int *tris, double DAMP_STIFF, double BEND_STIFF)
-{
-	Bend bend(cloth, tris);
-
-	double E = .5 * BEND_STIFF * bend.c *bend.c;
-	return E;
-}
